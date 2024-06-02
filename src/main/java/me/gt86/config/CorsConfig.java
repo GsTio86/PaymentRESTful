3e054aa38def20b9b4b4f9fb66513d98ab60912f
gstio86 <bailung20000218@gmail.com>
2024-06-03 00:33:57 +0800

    新增CORS的訪問設定

--- src/main/java/me/gt86/config/CorsConfig.java
+++ src/main/java/me/gt86/config/CorsConfig.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -24,15 +25,21 @@ public class CorsConfig {
         logger.info("Configuring CORS with allowed origins: {}", Arrays.toString(allowedOrigins));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
+        CorsConfiguration apiConfig = new CorsConfiguration();
 
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList(allowedOrigins));
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
+        apiConfig.setAllowCredentials(true);
+        apiConfig.setAllowedOrigins(Arrays.asList(allowedOrigins));
+        apiConfig.addAllowedHeader("*");
+        apiConfig.addAllowedMethod(HttpMethod.POST);
 
-        source.registerCorsConfiguration("/player/**", config);
-        source.registerCorsConfiguration("/payment/**", config);
+        source.registerCorsConfiguration("/player/**", apiConfig);
+        source.registerCorsConfiguration("/payment/**", apiConfig);
+
+        CorsConfiguration globalConfig = new CorsConfiguration();
+        globalConfig.setAllowCredentials(true);
+        globalConfig.addAllowedHeader("*");
+        globalConfig.addAllowedMethod(HttpMethod.POST);
+        source.registerCorsConfiguration("/**", globalConfig);
 
         return new CorsFilter(source);
     }
